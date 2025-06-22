ecaa9aa200086fb5ff724b4e96b01a5120a3dfd8
Catherine <consolatacatherinenjoki@gmail.com>
2025-06-22 19:44:50 +0300

    Update Catherine- Create.py

--- Catherine- Create.py
+++ Catherine- Create.py
@@ -1,5 +1,6 @@
 import mysql.connector
 new_medicines=[]
+
 def create_medicine(name,quantity,price,expiry):
   new_medicines.append([name,quantity,price,expiry])
   try:
@@ -9,6 +10,7 @@ def create_medicine(name,quantity,price,expiry):
         passwd="",
         database="medicines"
      )
+    
      cursor=conn.cursor()
      sql = "INSERT INTO meddata  (Name,Quantity,Price,Expiry) VALUES (%s,%s,%s,%s)"
      values = (name,quantity,price,expiry)
@@ -16,8 +18,10 @@ def create_medicine(name,quantity,price,expiry):
      conn.commit()
 
      print(f"Successfully added medicine: {name}")
+    
   except mysql.connector.Error as err:
      print(f"Something went wrong: {err}")
+    
   finally:
      if conn.is_connected():
         conn.close()
